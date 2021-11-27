f43517e63a828d7bba991277e57ae233c698b652
emiliano cassoni <cassoniemiliano@gmail.com>
2021-11-27 19:48:33 -0300

    update components

---
+++ components/FormInput.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { FONTS, COLORS, SIZES } from "../constants";
+
+const FormInput = ({
+  containerStyle,
+  label,
+  placeholder,
+  inputStyle,
+  prependComponent,
+  appendComponent,
+  onChange,
+  secureTextEntry,
+  keyboardType = "default",
+  autoCompleteType = "off",
+  autoCapitalize = "none",
+  errorMsg = "",
+}) => {
+  return (
+    <View style={{ ...containerStyle }}>
+      {/* Label & Error Messages */}
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={{ color: COLORS.gray, ...FONTS.body4 }}> {label}</Text>
+        <Text style={{ color: COLORS.red, ...FONTS.body4 }}>{errorMsg}</Text>
+      </View>
+
+      {/* Text input */}
+      <View
+        style={{
+          flexDirection: "row",
+          height: 55,
+          paddingHorizontal: SIZES.padding,
+          margiTop: SIZES.base,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.lightGray1,
+        }}
+      >
+        {prependComponent}
+
+        <TextInput
+          style={{
+            flex: 1,
+            ...inputStyle,
+          }}
+          placeholder={placeholder}
+          placeholderTextColor={COLORS.gray}
+          secureTextEntry={secureTextEntry}
+          keyboardType={keyboardType}
+          autoCompleteType={autoCompleteType}
+          autoCapitalize={autoCapitalize}
+          onChangeText={(text) => onChange(text)}
+        />
+
+        {appendComponent}
+      </View>
+    </View>
+  );
+};
+
+export default FormInput;
--- components/index.js
+++ components/index.js
@@ -1,9 +1,17 @@
 import Header from "./Header";
 import HorizontalFoodCard from "./HorizontalFoodCard"
 import VerticalFoodCard from "./VerticalFoodCard"
+import TextButton from "./TextButton"
+import TextIconButton from "./TextIconButton"
+import FormInput from "./FormInput";
+import CustomSwitch from "./CustomSwitch";
 
 export {
     Header,
     HorizontalFoodCard,
-    VerticalFoodCard
+    VerticalFoodCard,
+    TextButton,
+    FormInput,
+    CustomSwitch,
+    TextIconButton
 }
