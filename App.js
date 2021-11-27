d9223c578586ac53f3c6c457c22672a13482f99b
emiliano cassoni <cassoniemiliano@gmail.com>
2021-11-27 19:51:07 -0300

    initialize project

--- App.js
+++ App.js
@@ -1,32 +1,90 @@
+// import React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { StatusBar } from "react-native";
+
+// import CustomDrawer from "./navigation/CustomDrawer";
+// import { COLORS } from "./constants";
+
+// import { createStore, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "./store/rootReducer";
+
+// const Stack = createStackNavigator();
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <NavigationContainer>
+//         <StatusBar barStyle="light-content" backgroundColor={COLORS.black} />
+//         <Stack.Navigator
+//           screenOptions={{
+//             headerShown: false,
+//           }}
+//           initialRouteName={"Home"}
+//         >
+//           <Stack.Screen name="Home" component={CustomDrawer} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </Provider>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
-
-import CustomDrawer from "./navigation/CustomDrawer";
-import { COLORS } from "./constants";
+import SplashScreen from "react-native-splash-screen";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import rootReducer from "./store/rootReducer";
 
-const Stack = createStackNavigator();
+
+import {
+  OnBoarding,
+  SignIn,
+  SignUp,
+  ForgotPassword,
+  Otp,
+  Home,
+} from "./screens";
+import CustomDrawer from "./navigation/CustomDrawer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+const Stack = createStackNavigator();
+
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <StatusBar barStyle="light-content" backgroundColor={COLORS.black} />
         <Stack.Navigator
           screenOptions={{
             headerShown: false,
           }}
-          initialRouteName={"Home"}
+          initialRouteName={"OnBoarding"}
         >
+          <Stack.Screen name="OnBoarding" component={OnBoarding} />
+
           <Stack.Screen name="Home" component={CustomDrawer} />
+
+          <Stack.Screen name="SignIn" component={SignIn} />
+
+          <Stack.Screen name="SignUp" component={SignUp} />
+
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+
+          <Stack.Screen name="Otp" component={Otp} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
